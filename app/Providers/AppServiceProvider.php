428ea2d390f4a834f5e5fd22c8c0db9784ebe386
StefanBatandjiev <stefan.batandjiev@gmail.com>
2025-05-27 18:29:41 +0200

    chore: init skeleton

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Providers;
+
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureDates();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+
+    /**
+     * Configure the application's dates.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the application's models.
+     */
+    private function configureModels(): void
+    {
+        Model::unguard();
+
+        Model::shouldBeStrict();
+
+        Model::automaticallyEagerLoadRelationships();
+    }
+}
