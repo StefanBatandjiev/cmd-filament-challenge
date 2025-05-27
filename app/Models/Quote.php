64bcd37cd4e893014d6703db7d91a95c938ab671
StefanBatandjiev <stefan.batandjiev@gmail.com>
2025-05-27 18:44:11 +0200

    feat: created Quote model and migration

---
+++ app/Models/Quote.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\QuoteStatus;
+use App\Enums\ServiceType;
+use Illuminate\Database\Eloquent\Model;
+
+class Quote extends Model
+{
+    protected $casts = [
+        'service' => ServiceType::class,
+        'booked_at' => 'datetime',
+        'duration' => 'integer',
+        'status' => QuoteStatus::class,
+    ];
+}
