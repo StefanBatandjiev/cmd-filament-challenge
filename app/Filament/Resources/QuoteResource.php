0a06c7c91367cbbea4bb14323fbf5dc24195eb30
StefanBatandjiev <stefan.batandjiev@gmail.com>
2025-05-27 19:46:20 +0200

    feat: created QuoteResource

---
+++ app/Filament/Resources/QuoteResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\QuoteResource\Pages;
+use App\Filament\Resources\QuoteResource\RelationManagers;
+use App\Models\Quote;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class QuoteResource extends Resource
+{
+    protected static ?string $model = Quote::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('service')
+                ->badge(),
+                Tables\Columns\TextColumn::make('booked_at')
+                ->dateTime(),
+                Tables\Columns\TextColumn::make('duration')
+                ->numeric(),
+                Tables\Columns\TextColumn::make('created_at')
+                ->dateTime()
+                ->toggleable(true, true)
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListQuotes::route('/'),
+            'create' => Pages\CreateQuote::route('/create'),
+            'edit' => Pages\EditQuote::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->topNavigation()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
@@ -39,8 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+
             ])
             ->middleware([
                 EncryptCookies::class,
